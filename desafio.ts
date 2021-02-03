0f3e76949682111729c55b3b06ebfc7816c060e6
XioSawin <42756313+XioSawin@users.noreply.github.com>
2021-02-02 23:29:18 -0300

    Add files via upload

---
+++ desafio.ts
@@ -0,0 +1,53 @@
+import express, {Application, Request, Response} from 'express';
+import { emitWarning } from 'process';
+
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
+
+function leerArchivo() {
+    let emptyArray: any[] = [];
+
+    try {
+        return fs.readFile('./producto.txt', 'utf-8').split('\n');
+            
+    } catch (err) {
+        console.log('Error de lectura. ', err);
+        return emptyArray;
+    }
+}
+
+let productos: any[] = leerArchivo();
+
+let viewsItem = 0;
+let viewsRandom = 0;
+
+app.get('/items', (req, res)=>{ //agregar cant de productos al .txt
+    viewsItem++;
+    res.json(productos);
+})
+
+app.get('/item-random', (req, res)=>{
+    viewsRandom++;
+    const randomElement = productos[Math.floor(Math.random() * productos.length)];
+
+    res.json(randomElement.title);
+})
+
+app.get('/visitas', (req, res)=>{
+
+    const visitas = {
+        items: viewsItem,
+        itemRandom: viewsRandom
+    };
+
+    res.json(visitas);
+
+})
+
+
+app.listen(8080, ()=>{
+    console.log('Server listening - Port 8080');
+})
